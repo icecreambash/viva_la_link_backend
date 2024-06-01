4c68a586f8f5b0817922b60db77b9fe24d0fdb4b
PIzdec <test@test.test>
2024-06-01 14:15:30 +0300

    S

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Ticket;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dd(Ticket::query()->count());
+    }
+}
