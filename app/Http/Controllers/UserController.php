ad5879620d6897c90a468605c7c60ba855767ce8
PIzdec <test@test.test>
2024-06-01 18:43:18 +0300

    Append uuid for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\LoginUserRequest;
+use App\Http\Requests\User\RegisterUserRequest;
+use App\Http\Resources\Users\UserResource;
+use App\Models\City;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
-class UserController extends Controller
+class UserController extends Controller implements HasMiddleware
 {
-    //
+
+    public static function middleware() : array
+    {
+        return [
+            new Middleware(
+                middleware: 'auth:sanctum',
+                except: [
+                    'login',
+                    'register'
+                ]
+            )
+        ];
+    }
+
+    /**
+     * @throws \Throwable
+     */
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+
+        throw_if(!$user instanceof User || !Hash::check($request->password, $user->password), ValidationException::withMessages(
+            [
+                'message' => 'Login or password is not correct'
+            ]
+        ));
+
+        return response()->json(
+            [
+                'message' => 'Login successfully',
+                '_token' => $user->createToken('*')->plainTextToken,
+            ],
+            201
+        );
+    }
+
+    public function register(RegisterUserRequest $request)
+    {
+        $body = $request->only(
+            [
+                'name',
+                'email',
+                'password'
+            ]
+        );
+
+        /**
+         * В 11-м Ларавеле теперь автоматически хешируется пароль =)
+         */
+
+        $user = User::create(
+            $body
+        );
+
+
+        return response()->json(
+            [
+                'message' => 'Successfully created user!',
+                '_token' => $user->createToken('*')->plainTextToken
+            ],
+            201
+        );
+    }
+
+    public function getMe(Request $request)
+    {
+        return UserResource::make(auth()->user());
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group(['prefix'=>'user'],function (){
+    Route::post('login',[UserController::class,'login']);
+    Route::post('register',[UserController::class,'register']);
+    Route::get('/',[UserController::class,'getMe']);
+});
